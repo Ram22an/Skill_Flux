edfa4ad07b490036b2275439b76091e402c5eda7
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-15 00:13:01 +0530

    Register Course model in admin with custom admin options

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Course
 # Register your models here.
+# admin.site.register(Course) or use decorator
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display=('title','publish_date')
+    list_filter=('status','access')
+    search_fields=('title','description')
+
+
