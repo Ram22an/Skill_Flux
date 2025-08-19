76e209735c003cbf0004f8bd713d9a1febd73f23
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-19 11:25:40 +0530

    Enhance CourseAdmin to include image display and additional fields

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Course
 # Register your models here.
 # admin.site.register(Course) or use decorator
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display=('title','publish_date')
-    list_filter=('status','access')
-    search_fields=('title','description')
+    fields=['title','description','publish_date','access','status','image','display_image']
+    readonly_fields=['display_image']
+    list_display=['title','publish_date','display_image']
+    list_filter=['status','access']
+    search_fields=['title','description']
 
-
+    def display_image(self,obj):
+        print(obj.image.url)
+        url=obj.image.url
+        return format_html(f"<img src='{url}' width=100/>")
