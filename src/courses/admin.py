aaed81c83259b8e8cb0f7a9a404f4dc0f5aeab15
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-19 11:33:44 +0530

    resized the image using cloudinary

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,4 @@
+from cloudinary import CloudinaryImage
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Course
@@ -12,9 +13,10 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields=['title','description']
 
     def display_image(self,obj):
-        print(obj.image.url)
         url=obj.image.url
-        return format_html(f"<img src='{url}' width=100/>")
+        cloudinary_id=str(obj.image)
+        cloudinary_html=CloudinaryImage(cloudinary_id).image(width=100)
+        return format_html(cloudinary_html)
     display_image.short_description="Current Image"
 
 
