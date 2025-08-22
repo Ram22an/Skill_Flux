f1a61b5e8e980e0c7a43298012eaf734270e7a45
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-23 02:22:01 +0530

    Add can_preview and status fields to Lesson model

--- src/courses/models.py
+++ src/courses/models.py
@@ -82,7 +82,12 @@ class Lesson(models.Model):
     course=models.ForeignKey(Course,on_delete=models.CASCADE)
     title=models.CharField(max_length=120)
     description=models.TextField(blank=True,null=True)
-
+    can_preview=models.BooleanField(default=False,help_text="if user does not have access to course, can they see this?")
+    status=models.CharField(
+        max_length=10,
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+    )
 
 
 
